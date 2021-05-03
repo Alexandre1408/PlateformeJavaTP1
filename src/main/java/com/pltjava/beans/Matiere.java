f0fd49cde66cebea8c2c444c8d6cbd467e01fca1
Adrien <adrien.ingand1@gmail.com>
2021-05-03 17:37:00 +0200

    Add id to beans + Collection class

--- src/main/java/com/pltjava/beans/Matiere.java
+++ src/main/java/com/pltjava/beans/Matiere.java
@@ -2,28 +2,37 @@ package com.pltjava.beans;
 public class Matiere {
 	
 	private String nomMatiere;
-	
 	private Float coeff;
+	private static long cpt = 0;
+	private long idMatiere;
 	
 	public Matiere(String nomMatiere,Float coeff) {
 		setNomMatiere(nomMatiere);
 		setCoeff(coeff);
+		cpt++;
+		idMatiere = cpt;
 	}
 	
 	public String getNomMatiere() {
 		return nomMatiere;
 	}
 
-	public void setNomMatiere(String nomMatiere) {
+	public void setNomMatiere(String nomMatiere) 
+	{
 		this.nomMatiere = nomMatiere;
 	}
 
-	public float getCoeff() {
+	public float getCoeff() 
+	{
 		return coeff;
 	}
 
 	public void setCoeff(float coeff) {
 		this.coeff = coeff;
+	}
+
+	public long getIdMatiere() {
+		return idMatiere;
 	}
 
 }
